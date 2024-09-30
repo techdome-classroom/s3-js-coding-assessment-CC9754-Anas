ad3d958c3389d4a44049f770083f407f0d345cb7
CC9754-Anas <anasseraj01@gmail.com>
2024-09-30 15:49:37 +0530

    done program 2

--- program2.js
+++ program2.js
@@ -3,7 +3,26 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+  const romanNum = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  let intValue = 0;
+  let i = 0;
+  while (i < s.length) {
+    if (romanNum[s[i]] < romanNum[s[i + 1]]) {
+      intValue += romanNum[s[i + 1]] - romanNum[s[i]];
+      i += 2;
+    } else {
+      intValue += romanNum[s[i]];
+      i++;
+    }
+  }
 };
 
 
