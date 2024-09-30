311ad337e1ae07ecad04f9d07dce78e2b312abb4
CC9754-Anas <anasseraj01@gmail.com>
2024-09-30 15:43:36 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -4,7 +4,7 @@
  */
 var romanToInt = function(s) {
     const romanNumerals = {
-      'I': 1, 'V': 5, 'I' 1 V 5 X 10 L 50 C 100 D 500 M 1000 '
+      'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M' 1000 '
     }
 };
 
