7732d8c311207e926bb6bb2c3f91d38e9daa7863
CC9754-Anas <anasseraj01@gmail.com>
2024-09-30 15:35:33 +0530

    done program 1

--- program1.js
+++ program1.js
@@ -2,10 +2,36 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    
+var isValid = function (s) {
+  let brackets = 0;
+  let curly = 0;
+  let bigBrackets = 0;
+  for (let i = 0; i < s.length; i++) {
+    switch (s[i]) {
+      case "(":
+        brackets += 1;
+        break;
+      case "{":
+        curly += 1;
+        break;
+      case "[":
+        bigBrackets += 1;
+        break;
+      case ")":
+        brackets -= 1;
+        break;
+      case "}":
+        curly -= 1;
+        break;
+      case "]":
+        bigBrackets -= 1;
+        break;
+      default:
+        break;
+    }
+  }
+  if (brackets !== 0 || curly !== 0 || bigBrackets != 0) return false;
+  return true;
 };
 
 module.exports = { isValid };
-
-
