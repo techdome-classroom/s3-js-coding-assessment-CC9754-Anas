d3ed0dc225e5db4a3879b08ac918d8ee8e35bde6
CC9754-Anas <anasseraj01@gmail.com>
2024-09-30 15:49:13 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -2,23 +2,27 @@
  * @param {string} s
  * @return {number}
  */
-var romanToInt = function(s) {
-    const romanNum = {
-      'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+var romanToInt = function (s) {
+  const romanNum = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  let intValue = 0;
+  let i = 0;
+  while (i < s.length) {
+    if (romanNum[s[i]] < romanNum[s[i + 1]]) {
+      intValue += romanNum[s[i + 1]] - romanNum[s[i]];
+      i += 2;
+    } else {
+      intValue += romanNum[s[i]];
+      i++;
     }
-    let intValue = 0;
-    let i = 0;
-    while(i < s.length){
-      if(romanNum[s[i]] < romanNum[s[i + 1]]){
-        intValue += romanNum[s[i + 1]] - romanNum[s[i]];
-        i += 2;
-      }
-      else{
-        intValue += romanNum[s[i]];
-        i++;
-      }
-    }
+  }
 };
 
-
-module.exports={romanToInt}
+module.exports = { romanToInt };
